dcc1e1c8fe1585e497064117fa96dd54bbe62400
James Burns <jfburns@gmail.com>
2011-12-28 12:20:47 -0800

    Changed from HTMLParser to mechanize

--- dokuwiki.py
+++ dokuwiki.py
@@ -1,71 +1,44 @@
 import urllib2
 import urllib
 import settings
+import mechanize
+import cookielib
+from BeautifulSoup import BeautifulSoup
+import html2text
 
-from HTMLParser import HTMLParser
-
-forms = {}
-class MyHTMLParser(HTMLParser):
-    def __init__(self):
-        self.capturetext = 0
-        self.reset()
-
-    def handle_starttag(self, tag, attrs):
-        global forms
-        try:
-            if tag == 'textarea':
-                if attrs[0][1] == 'wikitext':
-                    self.capturetext = 1
-            if attrs[1][0] == 'name':
-                if attrs[1][1] == 'sectok':
-                    forms['sectok'] = attrs[2][1]
-            if attrs[1][0] == 'name':
-                if attrs[1][1] == 'changecheck':
-                    forms['changecheck'] = attrs[2][1]
-        except IndexError:
-            pass
-    def handle_endtag(self, tag):
-        if self.capturetext == 1:
-            if tag == 'textarea':
-                self.capturetext = 0
-#        print "Encountered an end tag:",tag
-    def handle_data(self, data):
-        if self.capturetext == 1:
-            forms['wikitext'] = data
+def isEditForm(f):
+    try:
+        if f.attrs['id'] == 'dw__editform':
+            return True
+    except KeyError:
+        return False
 
 def changepage(pagename,newtext):
     values = {}
+    br = mechanize.Browser()
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
 
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    
     url = settings.wiki_url
 
     values['do'] = 'edit'
     values['rev'] = ''
     values['id'] = pagename
 
-    parser = MyHTMLParser()
-
     data = urllib.urlencode(values)
-    req = urllib2.Request(url,data)
 
-    try:
-        resp = urllib2.urlopen(req)
-        parser.feed(resp.read())
-    except urllib2.URLError as e:
-        print e.reason
+    br.open(url,data)
+    br.select_form(predicate=isEditForm)
+    br.form['wikitext'] = newtext
+    br.submit()
 
-    forms['id'] = pagename
-    forms['rev'] = ''
-    forms['prefix'] = '.'
-    forms['suffix'] = ''
-    forms['target'] = 'section'
-    forms['wikitext'] = newtext
-    forms['do[save]'] = 'Save'
-
-    data = urllib.urlencode(forms)
-    req = urllib2.Request(url,data)
-
-    try:
-        resp = urllib2.urlopen(req)
-        #print resp.read() #should verify save successful
-    except urllib2.URLError as e:
-        print e.reason
+if __name__ == '__main__':
+    newchangepage('test','testing')
